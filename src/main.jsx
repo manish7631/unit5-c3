9b631289923aa2ca3665080bb66f1d609d2b3849
manishfw13 <manish_fw13_347@masai.school>
2022-04-25 17:04:45 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
-
+import { AuthContextProvider } from './context/authcontext'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-  
+    <AuthContextProvider>
  <App />
+ </AuthContextProvider>
  
  
  </BrowserRouter>
