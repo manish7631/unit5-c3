b757d7377ea4b094405a113fc072ae33fb9b8d14
manishfw13 <manish_fw13_347@masai.school>
2022-04-25 16:22:59 +0530

    first commit

---
+++ src/components/EmployeeList.jsx
@@ -0,0 +1,24 @@
+ 
+
+export const EmployeeList = () => {
+    
+ 
+    
+ 
+        return (
+            <div className="list_container">
+              {/* On clicking this card anywhere, user goes to user details */}
+              <div className="employee_card">
+                 <h1></h1>
+                <img  className="employee_image" />
+                <span className="employee_name"> 
+                     </span>
+                <span className="employee_title"></span>
+              </div>
+            </div>
+          );
+      
+ 
+     
+     
+  };
